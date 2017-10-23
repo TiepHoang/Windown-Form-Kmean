9eb63dd5b03601d67888b1efce6da0e5b6bc2436
hoangtiep96na@gmail.com <hoangtiep96na@gmail.com>
2017-10-23 23:25:10 +0700

    1

---
+++ Windows_K-Means/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Windows_K_Means
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numK = new System.Windows.Forms.NumericUpDown();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numK)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 401);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // numK
+            // 
+            this.numK.Location = new System.Drawing.Point(12, 448);
+            this.numK.Name = "numK";
+            this.numK.Size = new System.Drawing.Size(154, 20);
+            this.numK.TabIndex = 1;
+            this.numK.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(91, 474);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(12, 474);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.BackColor = System.Drawing.SystemColors.WindowText;
+            this.txtLog.Location = new System.Drawing.Point(172, 419);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(611, 78);
+            this.txtLog.TabIndex = 3;
+            this.txtLog.Text = "";
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(59, 422);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
+            this.lblCount.TabIndex = 4;
+            this.lblCount.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Count: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 509);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.numK);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numK)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.NumericUpDown numK;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Windows_K-Means/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_K_Means
+{
+    public partial class Form1 : Form
+    {
+        static Graphics g_panel;
+        List<tDot> lstDot;
+        List<tDot> lstCenter;
+
+        public string _log(string message, bool isError = false)
+        {
+            message = string.Format("\r\n{0}>> {1}", DateTime.Now.ToShortTimeString(), message);
+            txtLog.Select(txtLog.Text.Length, message.Length);
+            txtLog.SelectionColor = isError ? Color.Red : Color.Green;
+            txtLog.AppendText(message);
+            return message;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g_panel = panel1.CreateGraphics();
+            lstDot = new List<tDot>();
+            lstCenter = new List<tDot>();
+            txtLog.ReadOnly = true;
+        }
+
+        private void panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (lstDot.Any(q => q.has(e.Location)))
+            {
+                _log("trùng" + e.Location, true);
+            }
+            else
+            {
+                lstDot.Add(new tDot(e.Location));
+                _log("paint" + e.Location);
+                lblCount.Text = lstDot.Count.ToString();
+            }
+        }
+
+        public class tDot
+        {
+            public Point Location { get; set; }
+            public Color color { get; set; }
+            public eType type { get; set; }
+
+            public tDot(Point location)
+            {
+                this.Location = location;
+                type = eType.Default;
+                color = Color.Black;
+                _paint();
+            }
+
+            public float KhoangCach(Point p)
+            {
+                return (float)Math.Sqrt(Math.Pow(Location.X - p.X, 2) + Math.Pow(Location.Y - p.Y, 2));
+            }
+
+            public void _paint(bool isCenter = false)
+            {
+                SolidBrush b = new SolidBrush(color);
+                g_panel.FillEllipse(b, Location.X, Location.Y, isCenter ? 15 : 9, isCenter ? 15 : 9);
+            }
+
+            public bool has(Point p)
+            {
+                return p.Equals(Location);
+            }
+
+            public void Clear(bool isCenter = false)
+            {
+                SolidBrush b = new SolidBrush(SystemColors.Control);
+                g_panel.FillEllipse(b, Location.X, Location.Y, isCenter ? 15 : 9, isCenter ? 15 : 9);
+            }
+
+            public tDot Update(tDot center)
+            {
+                color = center.color;
+                type = center.type;
+                _paint();
+                return this;
+            }
+
+            public int SelectCenter(List<tDot> lstCenter)
+            {
+                if (lstCenter == null || lstCenter.Count <= 0) return -1;
+                int id = 0;
+                float kc = KhoangCach(lstCenter[id].Location);
+                for (int i = 1; i < lstCenter.Count; i++)
+                {
+                    float k = KhoangCach(lstCenter[i].Location);
+                    if (kc > k)
+                    {
+                        kc = k;
+                        id = i;
+                    }
+                }
+                Update(lstCenter[id]);
+                return id;
+            }
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+            txtLog.ScrollToCaret();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            foreach (var item in lstDot)
+            {
+                item.Clear(true);
+            }
+            lstDot.Clear();
+            lblCount.Text = lstDot.Count.ToString();
+            panel1.BackColor = SystemColors.Control;
+            _log("Clear");
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            run_kMeans();
+        }
+
+        private void run_kMeans()
+        {
+            var rd = new Random();
+
+            bool isEnd = false;
+
+            int sW = panel1.Location.X;
+            int eW = panel1.Width + sW;
+            int sH = panel1.Location.Y;
+            int eH = panel1.Height + sH;
+
+            int k = (int)numK.Value;
+            int asd = rd.Next();
+            foreach (var item in lstCenter)
+            {
+                item.Clear(true);
+            }
+
+            lstCenter = new List<tDot>();
+            for (int i = 0; i < k; i++)
+            {
+                tDot ob = new tDot(new Point()
+                {
+                    X = rd.Next(sW, eW),
+                    Y = rd.Next(sH, eH)
+                });
+                ob.type = (eType)Enum.GetValues(typeof(eType)).GetValue(1 + k % 3);
+
+                //KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+                //KnownColor randomColorName = names[rd.Next(names.Length)];
+                //Color randomColor = Color.FromKnownColor(randomColorName);
+                Color[] c = new Color[] { Color.Green, Color.Red, Color.Blue, Color.Yellow };
+                asd = (asd + 1) % c.Length;
+                ob.color = c[asd];
+                ob._paint(true);
+                _log(string.Format("Center {0}: {1}", i, ob.Location.ToString()));
+                lstCenter.Add(ob);
+            }
+
+            int count = 0;
+
+            List<int> lstIndex = new List<int>();
+            for (int i = 0; i < lstDot.Count; i++)
+            {
+                lstIndex.Add(-1);
+            }
+
+            while (!isEnd)
+            {
+                isEnd = true;
+                count++;
+                _log("K-Means: " + count);
+
+                for (int i = 0; i < lstDot.Count; i++)
+                {
+                    int id = lstDot[i].SelectCenter(lstCenter);
+                    if (lstIndex[i] != id)
+                    {
+                        lstIndex[i] = id;
+                        isEnd = false;
+                    }
+                }
+
+                for (int i = 0; i < lstCenter.Count; i++)
+                {
+                    int sx = 0, sy = 0, cc = 0;
+                    for (int j = 0; j < lstIndex.Count; j++)
+                    {
+                        if (lstIndex[j] == i)
+                        {
+                            sx += lstDot[j].Location.X;
+                            sy += lstDot[j].Location.Y;
+                            cc++;
+                        }
+                    }
+                    if (cc <= 0) cc = 1;
+                    lstCenter[i].Clear(true);
+                    lstCenter[i].Location = new Point(sx / cc, sy / cc);
+                    lstCenter[i]._paint(true);
+                    _log(string.Format("Center_{0}: [{3} == {1} - {4}=={2}]", i, lstCenter[i].Location.X, lstCenter[i].Location.Y, sx / cc, sy / cc));
+                }
+
+                Thread.Sleep(1000);
+            }
+            _log("Done");
+        }
+    }
+
+    public enum eType
+    {
+        Default, Tron, Vuong, TamGiac
+    }
+}
